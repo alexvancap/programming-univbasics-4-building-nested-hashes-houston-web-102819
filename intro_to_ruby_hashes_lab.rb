0d73cd61e917af4c00571cde5ef3a2a25055bd3c
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-10 16:05:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,11 +17,10 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces => 4
 			{
-				:rent_in_dollars => {
-					:boss => {},
-					:owner => {},
-					:manager => {},
-					:employee => {}
+				:rent_in_dollars => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200
 				}
 			}
 		}
